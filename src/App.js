1c30ecdfc1b942eda14321a4dc0642e8181b0c44
Aqeel012 <aqeelahmadradhani@gmail.com>
2023-12-16 23:54:34 +0500

    quote and home secton added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import './App.css';
- import { Link, Route, Router, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import { Link, Route, Router, Routes } from 'react-router-dom';
 import Calculator from './components/Calculator';
-
+import Quote from './components/Quote';
 
 function App() {
   return (
     <>
     <div className='nav'>
+        <h1>Math Magician</h1>
         <ul>
+          <Link  className='ulitem' to='/'>Home</Link>
           <Link className='ulitem' to='calculator'>Calculator</Link>
+          <Link className='ulitem' to='/quote'>Quote</Link>
         </ul>
     </div>
     <Routes>
+      <Route path='/' element={<Home/>}/>
       <Route path='/calculator' element={<Calculator/>}/>
+      <Route path='/quote' element={<Quote/>}/>
     </Routes>
     </>
   );
---
+++ src/components/Quote.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Quote = () => {
+  return (
+        <div className="quotes">
+          <blockquote cite="https://random-math-quote-api.herokuapp.com/">
+          "Mathematics is the language in which God has written the universe."
+        </blockquote>
+          <blockquote cite="https://random-math-quote-api.herokuapp.com/">
+          "The only way to learn mathematics is to do mathematics."
+    </blockquote>
+        </div>
+      );
+}
+
+export default Quote
